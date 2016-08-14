09e454b3cea16544fd0df9d8f5881b7e56c9ad6b
Elia Schito <elia@schito.me>
2016-08-14 23:58:09 +0200

    Make Module#include? fully compliant

--- spec/filters/bugs/module.rb
+++ spec/filters/bugs/module.rb
@@ -80,8 +80,6 @@ opal_filter "Module" do
   fails "Module#extend_object on Class raises a TypeError if calling after rebinded to Class"
   fails "Module#include does not override existing constants in modules and classes"
   fails "Module#include doesn't include module if it is included in a super class"
-  fails "Module#include? raises a TypeError when no module was given"
-  fails "Module#include? returns true if the given module is included by self or one of it's ancestors"
   fails "Module#instance_method gives UnboundMethod method name, Module defined in and Module extracted from"
   fails "Module#instance_method raises a NameError if the method has been undefined"
   fails "Module#instance_method raises a TypeError if not passed a symbol"
